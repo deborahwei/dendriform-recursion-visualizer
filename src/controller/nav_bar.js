d783efe7dd1a894c93b306de27e5212e403e608a
Deborah Wei <dew.silvercloud@gmail.com>
2022-07-26 23:34:54 -0400

    Beautiful css done

--- src/controller/nav_bar.js
+++ src/controller/nav_bar.js
@@ -2,14 +2,22 @@ export default class NavBar {
     constructor() { 
         this.navArea = document.createElement('div')
         this.navArea.classList.add('nav-area')
+
+        this.topNav = document.createElement('div')
+        this.navArea.appendChild(this.topNav)
+        this.topNav.classList.add('top-nav')
         
         const title = document.createElement('h1')
         title.textContent = "DENDRIFORM"
-        this.navArea.appendChild(title)
+        this.topNav.appendChild(title)
+        
+        const subheading = document.createElement('h3')
+        subheading.textContent = "a recursion visualizer"
+        this.topNav.appendChild(subheading)
 
-        const name = document.createElement('h2')
-        name.textContent = "Deborah Wei "
-        this.navArea.appendChild(name)
+        const description = document.createElement('h3')
+        description.textContent = "/ˈdendrəˌfôrm/ : having the shape or form of a tree"
+        this.navArea.appendChild(description)
 
         this.socials = document.createElement('div')
         this.socials.classList.add('socials')
@@ -25,12 +33,20 @@ export default class NavBar {
             window.open("https://www.linkedin.com/in/deborah-wei-163b10152/")
         })
 
+        const personalSite = document.createElement('img')
+        personalSite.src = "assets/tree.png"
+        this.socials.appendChild(personalSite)
+        personalSite.addEventListener("click", () => {
+            window.open("")
+        })
+
         const gitHub = document.createElement('i')
         gitHub.classList.add("fa-brands", "fa-github-square", "fa-2xl")
         this.socials.appendChild(gitHub)
         gitHub.addEventListener("click", () => {
             window.open("https://github.com/deborahwei?tab=repositories")
         })
+
     }
 
     getDOMObject() { 
